86a9f76b1d75be30b0dec589e7790c3ecf34c7f6
Bruce Ness <brucen@edprop.com>
2017-03-15 09:34:28 -0400

    Login navigation bug fixed

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavigationService } from '../shared/navigation.service';
 import { Subscription } from 'rxjs/Rx';
 
@@ -7,21 +7,19 @@ import { Subscription } from 'rxjs/Rx';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent implements OnInit {
+export class NavComponent {
 
   public brandTitle:string = 'PARKFAST Validations';
-  private isLogged: boolean;
+  private isLogged: boolean = false;
   public account: string;
   subscription: Subscription;
 
   constructor(private _navService: NavigationService) { 
       this.subscription = _navService.loggedInConfirmed$.subscribe(account => {
         this.account = account;
+        console.log(this.account);
+        account.length > 0 ? this.isLogged = true : this.isLogged = false;
       });
-  }
-
-  ngOnInit() {
-    this.account !== undefined ? this.isLogged = true : this.isLogged = false;
   }
 
   ngOnDestroy() {
